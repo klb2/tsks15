6e7a75996e690b17198a90b72213b2a49c61c6ea
Karl-Ludwig Besser <karl-ludwig.besser@liu.se>
2025-08-30 22:47:28 +0200

    Add explanations to pre-whitening notebook

--- notebooks/lecture2/pre_whitening.py
+++ notebooks/lecture2/pre_whitening.py
@@ -1,13 +1,104 @@
 import marimo
 
-__generated_with = "0.14.13"
+__generated_with = "0.15.2"
 app = marimo.App(width="medium")
 
 
 @app.cell
 def _(mo):
-    mo.md(r"""# Pre-Whitening""")
+    mo.md(
+        r"""
+    # Pre-Whitening
+
+    _Author:_ Karl-Ludwig Besser (Linköping University, Sweden)
+
+
+    This notebook illustrates the pre-whitening concept to deal with colored noise.
+    In particular, we assume that the noise samples follow a jointly Gaussian distribution with known covariance matrix $\Gamma$.
+
+    In the following, we consider a simple two-dimensional example.
+    Under hypothesis $\mathcal{H}_1$, the observations $y$ are given by
+
+    \begin{equation*}
+    \begin{pmatrix}y_x\\ y_y\end{pmatrix} = \begin{pmatrix}b_x\\ b_y\end{pmatrix} + \begin{pmatrix}w_x\\ w_y\end{pmatrix}
+    \end{equation*}
+
+    where $w\sim\mathcal{N}(0, \Gamma)$ and $$\Gamma = \begin{pmatrix}\sigma^2_x & \rho\sigma_x\sigma_y\\ \rho\sigma_x\sigma_y & \sigma^2_y\end{pmatrix}$$
+    """
+    )
     return
+
+
+@app.cell
+def _(slider_cov_matrix):
+    slider_cov_matrix
+    return
+
+
+@app.cell
+def _(slider_b):
+    slider_b
+    return
+
+
+@app.cell
+def _(
+    X,
+    Y,
+    b,
+    b_transform,
+    md_figure,
+    mo,
+    pdf_orig,
+    pdf_transform,
+    plt,
+    slider_b,
+    slider_cov_matrix,
+):
+    _fig, _axs = plt.subplots(1, 2, squeeze=True)
+    _ax1, _ax2 = _axs
+    _ax1.axis("equal")
+    _ax2.axis("equal")
+    _ax1.set_title("Likelihood under $\\mathcal{H}_1$ with center $b$")
+    _ax1.contourf(X, Y, pdf_orig)
+    _ax1.scatter(*b, c="k")
+    _ax1.set_xlabel("First component $y_x$")
+    _ax1.set_ylabel("Second component $y_y$")
+    _ax2.set_title("Likelihood under $\\mathcal{H}_1$ with center $b'$")
+    _ax2.contourf(X, Y, pdf_transform)
+    _ax2.scatter(*b_transform, c="k")
+    _ax2.set_xlabel("Transformed first component $y_x'$")
+    _ax2.set_ylabel("Transformed second component $y_y'$")
+    _fig.tight_layout()
+
+    mo.hstack(
+        [
+            mo.mpl.interactive(_fig),
+            mo.vstack([mo.md(md_figure), slider_b, slider_cov_matrix]),
+        ],
+        widths=[1.75, 1],
+    )
+    return
+
+
+@app.cell
+def _():
+    md_figure = r"""
+    In the left figure, you can see the density of the density of $y$.
+    Its mean is $b$, and due to the correlation of $w_x$ and $w_y$, the iso-density lines are ellipses.
+
+    The right figure shows the density of the transformed (whitened) variable $y'$ with mean $b'$. As the covariance is the identity, the iso-density lines are circles around $b'.$
+    """
+    return (md_figure,)
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import numpy as np
+    from scipy import stats
+    import matplotlib.pyplot as plt
+    return mo, np, plt, stats
 
 
 @app.cell
@@ -23,8 +114,8 @@ def _(np):
 def _(mo):
     slider_b = mo.ui.array(
         [
-            mo.ui.slider(-3, 3, 0.1, -1, label="$b_x$"),
-            mo.ui.slider(-3, 3, 0.1, -2, label="$b_y$"),
+            mo.ui.slider(-3, 3, 0.1, 0, label="$b_x$"),
+            mo.ui.slider(-3, 3, 0.1, 0, label="$b_y$"),
         ],
         label="Point $b$",
     )
@@ -49,16 +140,6 @@ def _(np, slider_b):
 
 
 @app.cell
-def _(b, grid, np, slider_cov_matrix, stats):
-    _var_x, _var_y, _corr = slider_cov_matrix.value
-    _cov = _corr * np.sqrt(_var_x * _var_y)
-    cov_mat = np.array([[_var_x, _cov], [_cov, _var_y]])
-    rv_h1_orig = stats.multivariate_normal(mean=b, cov=cov_mat)
-    pdf_orig = rv_h1_orig.pdf(grid)
-    return cov_mat, pdf_orig
-
-
-@app.cell
 def _(b, cov_mat, grid, np, stats):
     _eigval, _eigvec = np.linalg.eigh(cov_mat)
     whitening_mat = _eigvec @ (np.identity(2) / np.sqrt(_eigval)) @ _eigvec.T
@@ -73,37 +154,13 @@ def _(b, cov_mat, grid, np, stats):
 
 
 @app.cell
-def _(slider_cov_matrix):
-    slider_cov_matrix
-    return
-
-
-@app.cell
-def _(X, Y, b, b_transform, mo, pdf_orig, pdf_transform, plt):
-    _fig, _axs = plt.subplots(1, 2, squeeze=True)
-    _ax1, _ax2 = _axs
-    _ax1.set_title("Likelihood under $\\mathcal{H}_1$ with center $b$")
-    _ax1.contourf(X, Y, pdf_orig)
-    _ax1.scatter(*b, c="k")
-    _ax1.set_xlabel("First component $y[1]$")
-    _ax1.set_ylabel("Second component $y[2]$")
-    _ax2.set_title("Likelihood under $\\mathcal{H}_1$ with center $b'$")
-    _ax2.contourf(X, Y, pdf_transform)
-    _ax2.scatter(*b_transform, c="k")
-    _ax2.set_xlabel("Transformed first component $y'[1]$")
-    _ax2.set_ylabel("Transformed second component $y'[2]$")
-    _fig.tight_layout()
-    mo.mpl.interactive(_fig)
-    return
-
-
-@app.cell
-def _():
-    import marimo as mo
-    import numpy as np
-    from scipy import stats
-    import matplotlib.pyplot as plt
-    return mo, np, plt, stats
+def _(b, grid, np, slider_cov_matrix, stats):
+    _var_x, _var_y, _corr = slider_cov_matrix.value
+    _cov = _corr * np.sqrt(_var_x * _var_y)
+    cov_mat = np.array([[_var_x, _cov], [_cov, _var_y]])
+    rv_h1_orig = stats.multivariate_normal(mean=b, cov=cov_mat)
+    pdf_orig = rv_h1_orig.pdf(grid)
+    return cov_mat, pdf_orig
 
 
 if __name__ == "__main__":
